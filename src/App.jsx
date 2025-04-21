88c58f0f9238393a8c291860ae565c9c9ebbb10c
Mercy <mercymumbe17@gmail.com>
2025-04-21 21:12:08 +0300

    added app header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react';
+// import Note from './components/Note';
+// import NoteForm from './components/NoteForm';
+// import SearchBar from './components/SearchBar';
+import './App.css';
 
 function App() {
- 
+  
 
   return (
-    <>
-      
-    </>
-  )
+   
+      <div className="header">
+        <h1>Notes App </h1>
+       
+      </div>
+    
+  );
 }
 
-export default App
+export default App;
